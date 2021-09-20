16b334d43933e03e832804570e103c8419a475fc
Ilya Uraksin <i.uraksin@gmail.com>
2021-09-20 09:02:43 +0400

    main

--- src/components/ShowItem.tsx
+++ src/components/ShowItem.tsx
@@ -9,10 +9,10 @@ const ShowItem: FC<ShowItemProps> = ({ show }) => {
 
 	return (
 		<tr>
-			<td scope="row"><img src={show.image.medium} className="image" alt="" /></td>
+			<td><img src={show.image.medium} className="image" alt="" /></td>
 			<td>{show.name}</td>
 			<td>{show.genres.join(', ')}</td>
-			<td>{show.runtime}</td>
+			<td>{show.runtime} min</td>
 			<td>{show.rating.average}</td>
 		</tr>
 	)
